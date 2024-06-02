bf77062d25728f6ad56fc0c295ecfb208e611839
Lucas Silva <saculdasilva@hotmail.com>
2024-06-02 20:11:24 +0100

    chore(global): Disable DaisyUI Themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: false,
+  },
+};
